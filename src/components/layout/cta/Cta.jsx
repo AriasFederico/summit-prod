a9e0cd10a696ef4f6401ccf49e4427039330cfd2
AriasFederico <fede.ariass33@gmail.com>
2026-06-16 19:05:47 -0300

    end app

--- src/components/layout/cta/Cta.jsx
+++ src/components/layout/cta/Cta.jsx
@@ -8,7 +8,7 @@ export const Cta = () => {
             <div className={styles.content}>
                 <TopSection text={'NO ESPERES MAS'} />
                 <h2>Transformá tu gestión en minutos</h2>
-                <p>Con Pricify, lleva el control de tus precios, optimiza tus márgenes de ganancia y simplifica la gestión de tu negocio. Disfruta de una plataforma fácil de usar, diseñada para adaptarse a tus necesidades y ayudarte a alcanzar tus objetivos. ¡Anímate a empezar y descubre cómo Pricify puede transformar tu negocio!</p>
+                <p>Con Pricify, lleva el control de tus precios, optimiza tus márgenes de ganancia y simplifica la gestión de tu negocio. Disfruta de una plataforma fácil de usar, diseñada para adaptarse a tus necesidades y ayudarte a alcanzar tus objetivos.</p>
                 <div className={styles.ctas}>
                     <ButtonCta variant='secondary' text={'Comenzar gratis'} icon={'arrowRight'} href={'/registrarse'} />
                 </div>
--- src/components/layout/description/Description.jsx
+++ src/components/layout/description/Description.jsx
@@ -11,7 +11,7 @@ export const Description = () => {
                 <div className={styles.content}>
                     <TopSection text={'MARGEN DE GANANCIA'} />
                     <h2>Calculadoras porcentuales.</h2>
-                    <p className={styles.text}>Ingresa el costo de tu producto y el margen que queres ganar. Pricify calcula al instante el precio de venta ideal y la ganancia neta, sin fórmulas ni hojas de cálculos.</p>
+                    <p className={styles.text}>Ingresá el costo de tu producto y el margen que queres ganar. Pricify calcula al instante el precio de venta ideal y la ganancia neta, sin fórmulas ni hojas de cálculos.</p>
                 </div>
                 <div className={styles.media}>
                     <img src={calculators} alt="" className={styles.img} />
@@ -20,7 +20,7 @@ export const Description = () => {
             <SectionLayout direction={'row_reverse'}>
                 <div className={styles.content}>
                     <TopSection text={'LISTA DE PRODUCTOS'} />
-                    <h2>Gestiona tu lista de precios.</h2>
+                    <h2>Gestioná tu lista de precios.</h2>
                     <p className={styles.text}>Cada cálculo que realizas puede agregarse directo a tu lista de productos. Lleva un registro claro de costos, precios y márgenes de todo tu catálogo en un solo lugar.</p>
                 </div>
                 <div className={styles.media}>
